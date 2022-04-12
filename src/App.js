52f8c0a36f051226981349d8318456fd802d8c5f
Bruno Milano <brn.milano@hotmail.com>
2022-04-11 22:48:00 -0300

    Configuração do projeto e organização dos componentes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Home from "./Pages/home";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
+
+// export const companyData = {
+//   telefone: "3123131",
+// };
+// {companyData.telefone}
