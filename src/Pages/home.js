978d8fb4267d19ec27b48b779f3e6a07cfb9fdd3
Bruno Milano <brn.milano@hotmail.com>
2022-04-12 14:14:44 -0300

    Ajustes na estilização da Page Home

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,10 +1,12 @@
 import Header from "../Components/Header";
+import Register from "../Components/Register";
 import styles from "./styles.module.scss";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Header />
+      <Register />
     </div>
   );
 }
