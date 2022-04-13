41160ea82444ee4572edf6ac263ea5e2ddc94027
Bruno Milano <brn.milano@hotmail.com>
2022-04-13 12:29:09 -0300

    Criação do componente Vídeo - Instalação do ReactPlayer - Estilização e responsivo do componente Vídeo

---
+++ src/Components/Video/index.js
@@ -0,0 +1,33 @@
+import { Box } from "@mui/system";
+import ReactPlayer from "react-player";
+import styles from "./styles.module.scss";
+import useIsMobile from "../../helpers/useIsMobile";
+
+export default function Video() {
+  const isMobile = useIsMobile({ size: 820 });
+
+  return (
+    <Box className={styles.container}>
+      <Box className={styles.content}>
+        <Box className={styles.videoWrapper}>
+          {isMobile ? (
+            <ReactPlayer
+              className={styles.videoPlayer}
+              width="100%"
+              controls={true}
+              url="https://www.youtube.com/watch?v=5m-cn7sNC3Y"
+            />
+          ) : (
+            <ReactPlayer
+              width={880}
+              height={495}
+              className={styles.videoPlayer}
+              controls={true}
+              url="https://www.youtube.com/watch?v=5m-cn7sNC3Y"
+            />
+          )}
+        </Box>
+      </Box>
+    </Box>
+  );
+}
