fc82ec4d7889a0aec3dfae3dad55c969beb3f640
Bruno Milano <brn.milano@hotmail.com>
2022-04-13 18:59:45 -0300

    Criação e estilização do carousel dentro do componente Values - Estilização para fazer carousel aparecer apenas na versão mobile - Responsivo do componente Values

--- src/Components/Values/index.js
+++ src/Components/Values/index.js
@@ -4,24 +4,57 @@ import Text from "../Text";
 import Innovation from "./Assets/innovation.png";
 import HighTech from "./Assets/highTech.png";
 import Force from "./Assets/force.png";
+import Carousel from "react-elastic-carousel";
 
 export default function Values() {
   return (
-    <Box className={styles.container}>
-      <Box className={styles.imageWrapper}>
-        <img className={styles.image} src={Innovation} alt="Inovação" />
-        <Text fontSize={32}>Inovação</Text>
+    <>
+      <Box className={styles.container}>
+        <Box className={styles.desktopWrapper}>
+          <Box className={styles.imageWrapper}>
+            <img className={styles.image} src={Innovation} alt="Inovação" />
+            <Text fontSize={32}>Inovação</Text>
+          </Box>
+
+          <Box className={styles.imageWrapper}>
+            <img
+              className={styles.image}
+              src={HighTech}
+              alt="Alta Tecnologia"
+            />
+            <Text fontSize={32}>Alta Tecnologia</Text>
+          </Box>
+
+          <Box className={styles.imageWrapper}>
+            <img className={styles.image} src={Force} alt="Força" />
+            <Text fontSize={32}>Força</Text>
+          </Box>
+        </Box>
       </Box>
 
-      <Box className={styles.imageWrapper}>
-        <img className={styles.image} src={HighTech} alt="Alta Tecnologia" />
-        <Text fontSize={32}>Alta Tecnologia</Text>
-      </Box>
+      <Box className={`${styles.mobileWrapper} values-container`}>
+        <Carousel enableSwipe={true} showArrows={false} enableAutoPlay>
+          <Box className={styles.imageWrapper}>
+            <img className={styles.image} src={Innovation} alt="Inovação" />
+            <Text fontSize={20}>Inovação</Text>
+          </Box>
 
-      <Box className={styles.imageWrapper}>
-        <img className={styles.image} src={Force} alt="Força" />
-        <Text fontSize={32}>Força</Text>
+          <Box className={styles.imageWrapper}>
+            <img
+              className={styles.image}
+              src={HighTech}
+              alt="Alta Tecnologia"
+            />
+            <Text fontSize={20}>Alta Tecnologia</Text>
+          </Box>
+
+          <Box className={styles.imageWrapper}>
+            <img className={styles.image} src={Force} alt="Força" />
+            <Text fontSize={20}>Força</Text>
+          </Box>
+        </Carousel>
+        ;
       </Box>
-    </Box>
+    </>
   );
 }
